d650d2f5ee7f1ea6ba7707848a5c76bfbb7bb539
Sunny Dev <lightdev05@gmail.com>
2023-09-21 07:41:32 +0100

    Create the initial nestjs backend

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { MessagesModule } from './messages/messages.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(MessagesModule);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+}
+bootstrap();
