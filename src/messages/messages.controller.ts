209856134cfdb60269e71384eb861a9a8ff05b21
Sunny Dev <lightdev05@gmail.com>
2023-09-22 10:20:43 +0100

    FIX: refactoring to use dependency injection

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -11,13 +11,7 @@ import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-  messagesService: MessagesService;
-
-  constructor() {
-    // DONT DO THIS ON REAL APP
-    // USE DEPENDENCY INJECTION
-    this.messagesService = new MessagesService();
-  }
+  constructor(public messagesService: MessagesService) {}
 
   @Get()
   listMessages() {
